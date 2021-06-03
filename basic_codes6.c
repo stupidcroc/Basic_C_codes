7be68d7b710942607e7c8853c4b85419b18e3458
stupidcroc <51049725+stupidcroc@users.noreply.github.com>
2021-06-03 22:39:27 +0530

    swapping of 2 numbers, by 2 logics

---
+++ basic_codes6.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+int main()
+{
+    unsigned int a, b, c, d;
+    printf("\n Enter the first no. to swap = \n");
+    scanf("%u", &a);
+    printf("\n Enter the second no. to swap = \n");
+    scanf("%u", &b);
+    /* call swapfun1-> uses arithmetic operations to swap 2 nos good when memory consumption is less*/
+    swapfun1(a,b);
+
+    /* call swapfun2-> uses temporary varible to swap 2 nos good when instructions cycles are to be less*/
+    swapfun2(a,b);
+    return 0;
+}
+
+int swapfun1(int num1, int num2)
+{
+    unsigned int e, f;
+    e = num1;
+    f = num2;
+    e = e + f;
+    f = e - f;
+    e = e - f;
+    printf("\n swapped nos by method1 are = %u and %u", e, f);
+    return 0;
+    
+}
+
+int swapfun2(int num4, int num5)
+{
+    unsigned int g, h, num3;
+    g = num4;
+    h = num5;
+    num3 = g;
+    g = num5;
+    h = num3;
+    printf("\n swapped nos by method2 are = %u and %u", g, h);
+    return 0;
+    
+}
