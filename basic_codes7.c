e75b5c0f0394fffe2f140605c25f8022633543d7
stupidcroc <51049725+stupidcroc@users.noreply.github.com>
2021-06-04 12:04:44 +0530

    Added the digit separation function

--- basic_codes7.c
+++ basic_codes7.c
@@ -11,6 +11,26 @@ int main()
     result2 = ch1 - 32;
     printf("\n result_upper_to_lower = %c", result1);
     printf("\n result_lower_to_upper = %c", result2);
+	func2();
+
+    return 0;
+}
+
+
+int func2()
+{
+    /*Seperation of the digits*/
+    unsigned int digit, d1, d2, d3, d4;
+    printf("\n Enter 4 digit = \n");
+    scanf("%u", &digit);
+    d1 = digit % 10;
+    digit = digit / 10;
+    d2 = digit % 10;
+    digit = digit / 10;  
+    d3 = digit % 10;
+    digit = digit / 10;
+    d4 = digit;
+    printf("\n seperate nos are %u %u %u %u",d4, d3, d2, d1);
 
     return 0;
 }
