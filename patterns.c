7ab78f4844f4c4ba57e51c96157ddd0e2425325e
stupidcroc <51049725+stupidcroc@users.noreply.github.com>
2021-06-08 04:24:54 +0530

    Updated new patterns

--- patterns.c
+++ patterns.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 int pattern1();
+int spacepattern();
+int pattern2();
+
 int main()
 {
     /*Printing patterns*/
     pattern1();
+    spacepattern();
+    pattern2();
+
 
     return 0;
 }
+
 int pattern1(){
     int i, j;
     for(i = 0; i < 5; i++)
@@ -19,4 +26,27 @@ int pattern1(){
     }
     return 0;
 
+}
+
+int spacepattern(){
+    int i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+int pattern2(){
+    int i, j;
+    for(i = 0; i < 5; i++){
+        for(j = i; j < 5; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
 }
